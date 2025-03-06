97e5d29817020132b3c3451610f20453f2e1e214
edudscrc <eduardo.sa.coelho@gmail.com>
2025-03-05 22:37:37 -0300

    setup symlinks and relative paths

--- HyprlandScripts/screenshot_selected_area.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-grim -g "$(slurp -w 0)" - | tee ~/Pictures/screenshots/screenshot_$(date +'%Y-%m-%d_%H-%M-%S').png | wl-copy
-notify-send "Screenshot saved on ~/Pictures/screenshots/" "Copied to clipboard"
